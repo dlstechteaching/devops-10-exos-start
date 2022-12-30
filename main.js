bde79d0b2e2455b38f34984203b41dea82cb85bc
NJKatorz <nathanesakwa@gmail.com>
2022-12-30 18:51:32 +0100

    commit

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
+/* eslint-disable semi */
 (()=>{try{document.getElementById("month").innerText=((e=new Date)=>["January","February","March","April","May","June","July","August","September","October","November","December"][e.getMonth()])()}catch(e){}})();
