fe2f84c64fdd286707d403db4e3b124fc27782e1
chehr <chehrazad.ouazzani@student.vinci.be>
2023-01-19 23:55:27 +0100

    refactor(services): simplifying the excludeNamesWithX function

--- src/services/__tests__/excludeNamesWithX.test.js
+++ src/services/__tests__/excludeNamesWithX.test.js
@@ -6,18 +6,18 @@ describe("excludeNamesWithX tests suites", () => {
         expect(result).toEqual([]);
     });
 
-    it('should return an array with all names as no names have "x" letter', () =>{
-        const result = excludeNamesWithX(['Momo','Hadji','Leo']);
-        expect(result).toEqual(['Momo','Hadji','Leo']);
+    it('should return an array with all names as no names have "x" letter', () => {
+        const result = excludeNamesWithX(["Momo", "Hadji", "Leo"]);
+        expect(result).toEqual(["Momo", "Hadji", "Leo"]);
     });
 
-    it('should return an array with JeanKevin only as it is the only name without the letter X', () =>{
-        const result = excludeNamesWithX(['DMX','Xavier','JeanKevin']);
-        expect(result).toEqual(['JeanKevin']);
+    it("should return an array with JeanKevin only as it is the only name without the letter X", () => {
+        const result = excludeNamesWithX(["DMX", "Xavier", "JeanKevin"]);
+        expect(result).toEqual(["JeanKevin"]);
     });
 
-    it('should return an array with JC and Leo only as it is xena with the letter x', () =>{
-        const result = excludeNamesWithX(['JC','xena','Leo']);
-        expect(result).toEqual(['JC','Leo']);
+    it("should return an array with JC and Leo only as it is xena with the letter x", () => {
+        const result = excludeNamesWithX(["JC", "xena", "Leo"]);
+        expect(result).toEqual(["JC", "Leo"]);
     });
 });
