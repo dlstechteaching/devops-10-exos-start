1ca5deb1e3d8c06b1fa0876f081c13621c2b12e3
chehr <chehrazad.ouazzani@student.vinci.be>
2023-01-19 23:46:25 +0100

    test(services): test scenario when only one name is remaining

--- src/services/excludeNamesWithX.js
+++ src/services/excludeNamesWithX.js
@@ -1,3 +1,9 @@
 module.exports = function (names) {
-    return names;
+    const updateNames = [];
+    names.forEach(name => {
+        if(name.indexOf('X') === -1){
+            updateNames.push(name);
+        }
+    });
+    return updateNames;
 };
