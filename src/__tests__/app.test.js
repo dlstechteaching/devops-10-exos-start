74a779e44b46011c35982876be05bfde5750e262
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2023-01-19 21:20:01 +0100

    feat(test should return false): test should return false

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,5 +1,6 @@
 const { getCurrentMonth } = require("../app");
 const { isAdmin } = require("../app");
+const {seachWord} = require("../searchWord");
 
 jest.useFakeTimers().setSystemTime(new Date("2020-01-01"));
 
@@ -19,5 +20,13 @@ describe("app tests suites - isAdmin", () => {
     test("should return true as user is admin", () => {
         const result = isAdmin("admin");
         expect(result).toBe(true);
+    });
+});
+
+describe("test a function that removes all words containing the letter x", () => {
+    test("should return false when function doesn't exist", ()=> {
+        const tab = [''];
+        const reponse = seachWord(tab);
+        expect(reponse).toBe(['']);
     });
 });
